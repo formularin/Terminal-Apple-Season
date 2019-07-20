7ef3fc23e5edd3bd4ab64649f28f4857f2f7d37c
lol-cubes <arinmkhare@gmail.com>
2019-07-20 12:07:40 -0400

    updated BASKET_IMAGE to Image object

--- apple_season/basket.py
+++ apple_season/basket.py
@@ -6,11 +6,20 @@ cwd = dirname(dirname(__file__))
 if cwd not in sys.path:
     sys.path.append(cwd)
 
-from apple_season.coords import Coords
+from apple_season.coords import Coords, Image, Char
 
 
 with open(join(cwd, 'images/basket.txt'), 'r') as f:
-    BASKET_IMAGE = f.read()
+    basket_string = f.read()
+
+basket_grid = [list(row) for row in basket_string.split('\n')]
+
+chars = []
+for y, row in enumerate(basket_grid):
+    for x, char in enumerate(row):
+        chars.append(Char(x, y, char))
+
+BASKET_IMAGE = Image(chars)
 
 
 class Basket(Coords):
